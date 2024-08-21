e1a9a4191cf9f5e20b61c297dd24b6e6aa6c7011
AllaNovytska <anovytska2022@gmail.com>
2024-08-21 16:46:37 +0200

    maleFemale

--- script.js
+++ script.js
@@ -354,3 +354,24 @@ clearBtn.addEventListener("click", () => {
   clearList();
   wishArr = [];
 });
+
+
+
+
+document.getElementById('nameForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = document.getElementById('nameInput').value;
+  fetch(`https://api.genderize.io/?name=${name}`)
+      .then(response => response.json())
+      .then(data => {
+          const resultDiv = document.getElementById('name-result');
+          if (data.gender) {
+              resultDiv.textContent = `Имя: ${data.name}, Пол: ${data.gender}, Вероятность: ${data.probability}`;
+          } else {
+              resultDiv.textContent = 'Пол не определен для этого имени.';
+          }
+      })
+      .catch(error => {
+          console.error('Ошибка:', error);
+      });
+});
